7ef9462ac5da42725ac39ed5c753220fa4b67ac2
Tyrel <tyrelclayton92@gmail.com>
2019-01-02 19:42:44 -0500

    fix replyTo bug, and move index routes into separate file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Require dependencies
 var express = require("express"); 
 var app = express(); 
 var bodyParser = require("body-parser"); 
@@ -5,19 +6,21 @@ var expressSession = require("express-session");
 var flash = require("connect-flash"); 
 var nodemailer = require("nodemailer"); 
 
-var port = process.env.PORT || 3000; 
+// import routes
+var indexRoutes = require("./routes/index"); 
 
+// Set up default app settings
 app.use(express.static(__dirname + "/public")); 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
 
 app.use(expressSession({
-    secret: "Is it really a secret if it's in this file?", 
+    secret: process.env.EXPRESS_SECRET, 
     resave: false, 
     saveUninitialized: false
-}))
+}));
 
+// Set up flash message framework
 app.use(flash()); 
 app.use(function(req, res, next) {
     res.locals.error = req.flash("error"); 
@@ -25,45 +28,16 @@ app.use(function(req, res, next) {
     next(); 
 });
 
-app.get("/", function(req, res) {
-    res.render("index"); 
-});
+// Set up the route prefixes
+app.use("/", indexRoutes); 
 
-app.get("/contact", function(req, res) {
-    res.render("contact"); 
-})
-
-app.post("/contact", function(req, res) {
-    var smtpTransport = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.EMAILPW
-        }
-    });
-    var mailOptions = {
-        from: 'Tyrel Clayton <' + process.env.GMAIL + '>',
-        to: process.env.EMAIL,
-        replyTo: req.body.email,
-        subject: req.body.subject, 
-        text: "Someone reached out to you at tyrelclayton.com! \n\nName: " + req.body.name + "\n\nEmail: " + req.body.email + "\n\nMessage:\n\n" + req.body.message,
-        html: "<h3>Someone reached out to you at tyrelclayton.com!</h3><ul><li><strong>Name:</strong> " + req.body.name + "</li><li><strong>Email:</strong> " + req.body.email + "</li></ul><p>Message:<br/><br/>" + req.body.message + "</p>"
-    };
-
-    smtpTransport.sendMail(mailOptions, function(err, info) {
-        if (err) {
-            console.log(err); 
-            req.flash("error", "Something went wrong. Please try again or reach out to tyrel@tyrelclayton.com"); 
-            res.redirect("/contact");
-        }
-        req.flash("success", "Your message was sent! Tyrel should be in touch soon!");
-        res.render("index"); 
-    });
-});
-
+// Set up the error 404 page
 app.use(function(req, res, next) {
     res.status(404).render("not_found"); 
 });
+
+// Decide on a port, then listen for requests
+var port = process.env.PORT || 3000; 
 
 app.listen(port, function() {
     console.log("The server has started!"); 
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require("express"); 
+var router = express.Router(); 
+var nodemailer = require("nodemailer"); 
+
+
+
+router.get("/", function(req, res) {
+    res.render("index"); 
+});
+
+router.get("/contact", function(req, res) {
+    res.render("contact"); 
+})
+
+router.post("/contact", function(req, res) {
+    var smtpTransport = nodemailer.createTransport({
+        service: "Yahoo",
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAILPW
+        }
+    });
+
+    var mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.ALIAS,
+        replyTo: req.body.email,
+        subject: req.body.subject, 
+        text: "Someone reached out to you at tyrelclayton.com! \n\nName: " + req.body.name + "\n\nEmail: " + req.body.email + "\n\nMessage:\n\n" + req.body.message,
+        html: "<h3>Someone reached out to you at tyrelclayton.com!</h3><ul><li><strong>Name:</strong> " + req.body.name + "</li><li><strong>Email:</strong> " + req.body.email + "</li></ul><p>Message:<br/><br/>" + req.body.message + "</p>"
+    };
+
+    smtpTransport.sendMail(mailOptions, function(err, info) {
+        if (err) {
+            console.log(err); 
+            req.flash("error", "Something went wrong. Please try again or reach out to tyrel@tyrelclayton.com"); 
+            res.redirect("/contact");
+        }
+        req.flash("success", "Your message was sent! Tyrel should be in touch soon!");
+        res.redirect("/"); 
+    });
+});
+
+module.exports = router;
