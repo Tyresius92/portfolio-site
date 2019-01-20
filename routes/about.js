bfbc78a2d15cfa246bb854306d1ba55baf67aea2
Tyrel <tyrelclayton92@gmail.com>
2019-01-19 22:10:04 -0500

    update about content and add alternate background pngs

--- routes/about.js
+++ routes/about.js
@@ -5,6 +5,10 @@ router.get("/", function(req, res) {
     res.render("about/about"); 
 });
 
+router.get("/deep-dive", function(req, res) {
+    res.render("about/deep"); 
+});
+
 router.get("/impossible-list", function(req, res) {
     res.render("about/impossible"); 
 });
